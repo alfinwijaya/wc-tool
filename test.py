659f99db0b16521af098f278d659a7a56a8de9fd
Alfin <alfin.wijaya@agungsedayu.com>
2023-11-21 09:11:35 +0700

    updated test case

--- test.py
+++ test.py
@@ -7,55 +7,55 @@ class TestCCWCScript(unittest.TestCase):
     
         result = subprocess.run(cmd_args, capture_output=True, text=True)
 
-        expected_output = '342190 test.txt'
-        self.assertIn(expected_output, result.stdout)
+        expected_output = '342190 test.txt\n'
+        self.assertEqual(expected_output, result.stdout)
 
     def test_count_lines(self):
         cmd_args = ['python', 'ccwc.py', '-l', 'test.txt']
     
         result = subprocess.run(cmd_args, capture_output=True, text=True)
 
-        expected_output = '7145 test.txt'
-        self.assertIn(expected_output, result.stdout)
+        expected_output = '7145 test.txt\n'
+        self.assertEqual(expected_output, result.stdout)
 
     def test_count_words(self):
         cmd_args = ['python', 'ccwc.py', '-w', 'test.txt']
     
         result = subprocess.run(cmd_args, capture_output=True, text=True)
 
-        expected_output = '58164 test.txt'
-        self.assertIn(expected_output, result.stdout)
+        expected_output = '58164 test.txt\n'
+        self.assertEqual(expected_output, result.stdout)
 
     def test_count_characters(self):
         cmd_args = ['python', 'ccwc.py', '-m', 'test.txt']
     
         result = subprocess.run(cmd_args, capture_output=True, text=True)
 
-        expected_output = '339292 test.txt'
-        self.assertIn(expected_output, result.stdout)
+        expected_output = '339292 test.txt\n'
+        self.assertEqual(expected_output, result.stdout)
 
     def test_using_all_options(self):
         cmd_args = ['python', 'ccwc.py', '-c', 'test.txt', '-l', 'test.txt', '-w', 'test.txt', '-m', 'test.txt']
     
         result = subprocess.run(cmd_args, capture_output=True, text=True)
 
-        expected_output = '342190 7145 58164 339292 test.txt'
-        self.assertIn(expected_output, result.stdout)
+        expected_output = '342190 7145 58164 339292 test.txt\n'
+        self.assertEqual(expected_output, result.stdout)
 
     def test_no_optional_args(self):
         cmd_args = ['python', 'ccwc.py', 'test.txt']
     
         result = subprocess.run(cmd_args, capture_output=True, text=True)
 
-        expected_output = '342190 7145 58164 test.txt'
-        self.assertIn(expected_output, result.stdout)
+        expected_output = '342190 7145 58164 test.txt\n'
+        self.assertEqual(expected_output, result.stdout)
 
     def test_multiple_files(self):
         cmd_args = ['python', 'ccwc.py', '-c', 'test.txt', '-l', 'test.txt', '-c', 'test2.txt']
     
         result = subprocess.run(cmd_args, capture_output=True, text=True)
 
-        # I make two version because the order can be inconsistent
+        # I make two versions because the order can be inconsistent
         expected_output = [ f'342190 7145 test.txt\n504 test2.txt\n342694 7145 total\n',
                             f'504 test2.txt\n342190 7145 test.txt\n342694 7145 total\n']
         self.assertIn(result.stdout, expected_output)
@@ -65,8 +65,8 @@ class TestCCWCScript(unittest.TestCase):
     
         result = subprocess.run(cmd_args, capture_output=True, text=True)
 
-        expected_output = f'ccwc: test3.txt: No such file or directory'
-        self.assertIn(expected_output, result.stdout)
+        expected_output = f'ccwc: test3.txt: No such file or directory\n'
+        self.assertEqual(expected_output, result.stdout)
 
     def test_using_standard_input(self):
         ps_command =  (
@@ -78,8 +78,8 @@ class TestCCWCScript(unittest.TestCase):
 
         self.assertEqual(result.returncode, 0, f"Command failed with return code {result.returncode}")
 
-        expected_output = "342190 7145 58164 339292"
-        self.assertIn(expected_output, result.stdout)
+        expected_output = "342190 7145 58164 339292\n"
+        self.assertEqual(expected_output, result.stdout)
 
     def test_using_standard_input_without_args(self):
         ps_command =  (
@@ -91,8 +91,8 @@ class TestCCWCScript(unittest.TestCase):
 
         self.assertEqual(result.returncode, 0, f"Command failed with return code {result.returncode}")
 
-        expected_output = "342190 7145 58164"
-        self.assertIn(expected_output, result.stdout)
+        expected_output = "342190 7145 58164 \n"
+        self.assertEqual(expected_output, result.stdout)
 
 if __name__ == "__main__":
     unittest.main()
